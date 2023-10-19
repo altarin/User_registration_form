3610d3e574054a1421bfdfdbe765494300b3ecc4
Natalia <alt.antares@gmail.com>
2023-10-19 10:41:36 +0300

    added validation email

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './index.scss';
+
+
+//валидация email
+
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+const inputEmail = document.querySelector('input');
+let errorMessageEmail = document.getElementById('errorMessageEmail');
+
+function isEmailValid(value) {
+    return EMAIL_REGEXP.test(value);
+}
+
+function onInput() {
+
+	if (isEmailValid(inputEmail.value)) {
+		inputEmail.style.borderColor = 'green';
+	} else {
+		inputEmail.style.borderColor = 'red';
+        showError();
+    }
+}
+
+
+inputEmail.addEventListener('input', onInput);
+
+
+// function showError() {
+//     errorMessageEmail.textContent = '* Поле E-mail заполнено не верно';
+// }
